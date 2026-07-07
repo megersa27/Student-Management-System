95300c0ab9f582fb9cf4b1b40e1ed8da9b4d3cc7
megersa27 <megersatek95@gmail.com>
2026-07-07 14:11:50 +0300

    Completed Milestone 5 delete student

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,10 +1,38 @@
-function StudentCard({ student }) {
+function StudentCard({
+  student,
+  deleteStudent
+}) {
   return (
     <div className="student-card">
+
       <h3>{student.name}</h3>
-      <p>ID: {student.id}</p>
-      <p>Department: {student.department}</p>
-      {student.department==="CSE" ? "Excellent Student" : ""}
+
+      <p>
+        Student ID:
+        {student.studentId || student.id}
+      </p>
+
+      <p>
+        Department:
+        {" "}
+        {student.department}
+      </p>
+
+      {student.department ===
+        "CSE" && (
+        <p>
+          Excellent Student ⭐
+        </p>
+      )}
+
+      <button
+        onClick={() =>
+          deleteStudent(student.id)
+        }
+      >
+        Delete
+      </button>
+
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 import StudentCard from "../components/StudentCard";
 
 function Home() {
-  const [students] = useState([
+  const [students, setStudents] = useState([
     {
       id: 1,
       name: "John Doe",
@@ -13,37 +13,117 @@ function Home() {
     },
     {
       id: 2,
-      name: "Abebe Kebede",
-      department: "Software Engineering",
-    },
-    {
-      id: 3,
       name: "Sara Ali",
       department: "IT",
     },
-    {
-      id: 4,
-      name: "Megersa Tekalign",
-      department: "CSE",
-    },
   ]);
+
+  const [name, setName] = useState("");
+  const [studentId, setStudentId] = useState("");
+  const [department, setDepartment] = useState("");
+  const [email, setEmail] = useState("");
+
+  const addStudent = () => {
+    if (!name || !studentId || !department) {
+      alert("Please fill all fields");
+      return;
+    }
+    const existingStudent = students.find(
+      (student) => student.studentId === studentId,
+    );
+
+    if (existingStudent) {
+      alert("Student ID already exists");
+      return;
+    }
+
+    const newStudent = {
+      id: Date.now(),
+      name,
+      email,
+      studentId,
+      department,
+    };
+
+    setStudents([...students, newStudent]);
+
+    setName("");
+    setEmail(" ");
+    setStudentId("");
+    setDepartment("");
+  };
+
+  const deleteStudent = (id) => {
+    if (
+  window.confirm(
+    "Delete this student?"
+  )
+){
+  const updatedStudents =
+    students.filter(
+      (student) => student.id !== id
+    );
+
+  setStudents(updatedStudents);
+}
+};
 
   return (
     <div className="app">
       <Navbar />
 
       <section className="hero">
-        <h1>ASTU Student Management System</h1>
+        <h1>Student Management System</h1>
         <p>Manage students easily.</p>
-        <button>Get Start</button>
       </section>
 
+      <div className="form-container">
+        <h2>Add Student</h2>
+
+        <input
+          type="text"
+          placeholder="Student Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Student ID"
+          value={studentId}
+          onChange={(e) => setStudentId(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Department"
+          value={department}
+          onChange={(e) => setDepartment(e.target.value)}
+        />
+
+        <button onClick={addStudent}>Add Student</button>
+      </div>
+
       <div className="students-container">
-        <h2>Students</h2>
-        <p> Total Students : {students.length}</p>
+        <h2>
+          Total Students:
+          {students.length}
+        </h2>
+
         <div className="student-list">
           {students.map((student) => (
-            <StudentCard key={student.id} student={student} />
+            <StudentCard
+              key={student.id}
+              student={student}
+              deleteStudent={deleteStudent}
+            />
           ))}
         </div>
       </div>
