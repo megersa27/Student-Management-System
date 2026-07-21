c205f01735cb887eefe77b9e4a45b3fe1b47fad9
megersa27 <megersatek95@gmail.com>
2026-07-21 12:38:00 +0300

    Add global theme using Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { useContext } from "react";
+
+import { ThemeContext } from "./context/ThemeContext";
 import StudentDetails from "./pages/StudentDetails";
 
 function App() {
+  const { theme } =
+  useContext(
+    ThemeContext
+  );
   return (
     <BrowserRouter>
+    <div className={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/student/:id" element={<StudentDetails />} />
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,33 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 function Navbar() {
+  const {
+  theme,
+  toggleTheme,
+} = useContext(
+  ThemeContext
+);
   return (
     <nav className="navbar">
-
-      <h2>
-        Student Management System
-      </h2>
+      <h2>Student Management System</h2>
 
       <div className="nav-links">
+        <a href="#">Home</a>
 
-        <a href="#">
-          Home
-        </a>
+        <a href="#">Students</a>
 
-        <a href="#">
-          Students
-        </a>
-
-        <a href="#">
-          Dashboard
-        </a>
-
+        <a href="#">Dashboard</a>
       </div>
+      <button
+  onClick={toggleTheme}
+>
 
+{theme === "light"
+? "🌙 Dark"
+: "☀️ Light"}
+
+</button>
     </nav>
   );
 }
